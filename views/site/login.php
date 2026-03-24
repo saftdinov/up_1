7aac3eea58245231e04629a3c453c7c57d49d3d8
saftdinov <saftdinov444@gmail.com>
2026-03-24 13:06:22 +0700

    начало интеграции интерфейса

--- views/site/login.php
+++ views/site/login.php
@@ -5,9 +5,31 @@
 <?php
 if (!app()->auth::check()):
     ?>
-    <form method="post">
-        <label>Логин <input type="text" name="login"></label>
-        <label>Пароль <input type="password" name="password"></label>
-        <button>Войти</button>
-    </form>
+    <!doctype html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="css/style.css">
+        <title>Document</title>
+    </head>
+    <body>
+    <div class="conteiner">
+        <form method="post">
+
+        <div class="login">
+            <label>Логин <input type="text" name="login"><br></label>
+        </div>
+        <div class="password">
+            <label>Пароль <input type="password" name="password"><br></label>
+        </div>
+        <div class="entrance">
+            <button>Войти</button>
+        </div>
+        </form>
+    </div>
+    </body>
+    </html>
 <?php endif;
